40e4987f0690305178a5ea2aa7ad5ef2fcf7d21e
ayushdharwal@gmail.com <ayushdharwal@gmail.com>
2025-08-13 21:07:20 +0530

    useState Hook

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,10 +1,31 @@
-
+import React,{useState} from "react"
 function App() {
+let [counter,setCounter]=useState(0)
+const addvalue=()=>{
+  setCounter(counter+1)
+}
+const subvalue=()=>{
+  if(counter>0){
+  setCounter(counter-1)
+}
+else {
+  setCounter(counter=0)
+}
+
+
+}
+
+  
   return (
     <>
-    <h1>Hello World</h1>
+    <h1>Counter Project</h1>
+    <h2>Counter: {counter}</h2>
+    <button onClick={addvalue}>+</button>
+    <button onClick={subvalue}>-</button> 
+    <p >{counter}</p>
     </>
   )
+  
 }
 
 export default App
