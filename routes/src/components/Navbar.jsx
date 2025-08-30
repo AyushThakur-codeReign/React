165530401e6d0d188c53a5dff0470f52239fcbd4
ayushdharwal@gmail.com <ayushdharwal@gmail.com>
2025-08-30 22:46:45 +0530

    done

--- routes/src/components/Navbar.jsx
+++ routes/src/components/Navbar.jsx
@@ -3,16 +3,16 @@ import {  NavLink } from 'react-router-dom'
 const Navbar = () => {
   return (
     <div>
-        <nav className='flex bg-gray-700 text-white gap-3 list-none '>
-            <NavLink className={(e)=>{
+        <nav className='flex bg-gray-700 text-white gap-4 list-none h-14'>
+            <NavLink className= {(e)=>{
                return e.isActive?"bg-red-800 ":""
-            }} to="/"><li>Home</li></NavLink>
+            }} to="/"><li className='p-4'>Home</li></NavLink>
             <NavLink className={(e)=>{
                return e.isActive?"bg-red-800":""
-            }} to="/about"><li>About </li></NavLink>
+            }} to="/about"><li className='p-4'>About </li></NavLink>
             <NavLink className={(e)=>{
                return e.isActive?"bg-red-800":""
-            }} to="/login"><li>login</li></NavLink>
+            }} to="/login"><li className='p-4'>login</li></NavLink>
        </nav>
     </div>
   )
